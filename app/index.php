92dc2213b55fda0d7c19d824f9008776dcec1683
Corey Weber <corey.weber11@gmail.com>
2016-07-06 16:31:12 -0300

    fuck slim

--- app/index.php
+++ app/index.php
@@ -1,7 +1,8 @@
 <?php
+/*
 require 'Slim/Slim.php';
 $app = new Slim();
-//require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 //use Mailgun\Mailgun;
 
 $app->post('/postBid', 'postBid');
@@ -20,11 +21,37 @@ function getConnection() {
 	$dbuser="";
 	$dbpass="";
 	$dbname="";
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
 }
 
 $app->run();//run
+
+*/
+
+$dbhost = "107.180.26.160";
+$dbuser = "jrholden";
+$dbpass = "Google92@";
+$dataBase = "AUCTIONTABLES";
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dataBase);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM auction_bids";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row["bids_id"];
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
 
 ?>
--- app/items/itemsCtrl.js
+++ app/items/itemsCtrl.js
@@ -3,53 +3,59 @@
  */
 'use strict';
 
-angular.module('myApp').controller('ItemsCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+angular.module('myApp').controller('ItemsCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
     console.log("We Are ITEMS");
 
-    $scope.setModal = function(item){
+    $scope.setModal = function (item) {
         $scope.modalItem = item;
         console.log($scope.modalItem);
     };
 
-    $scope.getItems = function(){
+    $scope.getItems = function () {
 
         //todo get items from db
         $scope.items = [{title: "Title 1"}, {title: "Title 2"}];
     };
 
-    $scope.makeBid = function(){
+    $scope.makeBid = function () {
 
-        if(/*$scope.name == undefined || $scope.name == ''*/false){
+        if (/*$scope.name == undefined || $scope.name == ''*/false) {
             alert("No Name");
-        }else{
+        } else {
             //$scope.bid = {name: $scope.name, email: $scope.email, phone: $scope.phone, bid: $scope.bid, itemId: $scope.itemId};
 
             $scope.bid = {name: "Corey Weber", email: "Test@Test.com", phone: "90299999999", bid: 1000000, itemId: 1};
 
             /*$http.post('index.php/postBid', $scope.bid).success(function(response){
-                console.log("not fed up!", response);
-            }).error(function(response){
-                console.log("fed up!");
-            });*/
+             console.log("not fed up!", response);
+             }).error(function(response){
+             console.log("fed up!");
+             });*/
 
-            $http.get('index.php/').success(function(data){
+            console.log("Testing");
+
+            $http.get("index.php")
+                .then(function (response) {
+                    console.log(response);
+                });
+
+            /*$http.get('index.php').success(function (data) {
                 console.log(data);
-            });
-
-//jnjj
-            /*$http({$app->run();//run
-                method: 'POST',
-                url: '../index.php/postBid',
-                data: $scope.bid,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            }).
-            success(function(response) {
-                $scope.codeStatus = response.data;
-            }).
-            error(function(response) {
-                $scope.codeStatus = response || "Request failed";
             });*/
+            
+            /*$http({$app->run();//run
+             method: 'POST',
+             url: '../index.php/postBid',
+             data: $scope.bid,
+             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+             }).
+             success(function(response) {
+             $scope.codeStatus = response.data;
+             }).
+             error(function(response) {
+             $scope.codeStatus = response || "Request failed";
+             });*/
             //todo add to db
             //http.post('/database.php', $scope.bid).then(successCallback, errorCallback);
         }
